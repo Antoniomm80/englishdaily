dc05e19eccd45e1d4ad62d0198c9b996ef31a0f1
AntonioMM <antoniomm80@gmail.com>
2025-01-09 16:58:48 +0100

    config: Now vector store setup relies on autoconfiguration

--- src/main/java/com/anmoma/englishdaily/vectorstore/VectorStoreConfiguration.java
+++ src/main/java/com/anmoma/englishdaily/vectorstore/VectorStoreConfiguration.java
@@ -1,29 +1,12 @@
 package com.anmoma.englishdaily.vectorstore;
 
-import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
-import org.springframework.ai.vectorstore.PgVectorStore;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import static org.springframework.ai.vectorstore.PgVectorStore.PgDistanceType.COSINE_DISTANCE;
-import static org.springframework.ai.vectorstore.PgVectorStore.PgIndexType.HNSW;
 
 @Configuration
 public class VectorStoreConfiguration {
-    @Value("${spring.ai.vectorstore.pgvector.initialize-schema}")
-    private boolean inializeSchema;
-
-    @ConditionalOnMissingBean
-    @Bean
-    PgVectorStore vectorStore(JdbcTemplate jdbcTemplate, EmbeddingModel embeddingModel) {
-
-        return new PgVectorStore(jdbcTemplate, embeddingModel, 768, COSINE_DISTANCE, false, HNSW, inializeSchema);
-    }
 
     @Bean
     TextSplitter tokenTextSplitter() {
